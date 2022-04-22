16d549831e63a851fbe3b1d122ddbd6c1485b42b
Bamidele Olawale Ajayi <81062134+baajayi@users.noreply.github.com>
2022-04-22 16:30:52 +0100

    Initialize team2

---
+++ src/js/utils.js
@@ -0,0 +1,27 @@
+// wrapper for querySelector...returns matching element
+export function qs(selector) {
+  return document.querySelector(selector);
+}
+
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+// save data to local storage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener('touchend', (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener('click', callback);
+}
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
