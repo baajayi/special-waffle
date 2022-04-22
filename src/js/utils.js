44c997922f62dbcaef19d334683b90e320c29d06
Bamidele Olawale Ajayi <81062134+baajayi@users.noreply.github.com>
2022-04-22 16:30:53 +0100

    Initialize team3

---
+++ src/js/utils.js
@@ -0,0 +1,35 @@
+// wrapper for querySelector...returns matching element
+export function qs(selector) {
+  return document.querySelector(selector);
+}
+
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+// save data to local storage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener('touchend', (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener('click', callback);
+}
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+export function renderListWithTemplate(template, parent, list, callback) {
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const templateWithData = callback(clone, item);
+    parent.appendChild(templateWithData);
+  })
+}
