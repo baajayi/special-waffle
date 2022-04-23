0bc81289a9d65d55486c5235f40f3f6463657804
Goosa2025 <gus21002@byui.edu>
2022-04-23 11:25:35 -0600

    Removed i = 0 line 1 because it's defined in line 7

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,43 +1,42 @@
-let i = 0
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
 function getCartContents() {
   //let markup = "";
-  for (let i = 0; i < localStorage.length; i++) { 
-    let cartItem = getLocalStorage(i+1);
+  for (let i = 0; i < localStorage.length; i++) {
+    let cartItem = getLocalStorage(i + 1);
     //const htmlItems = cartItems.map((item) => renderCartItem(item));
     //document.querySelector(".product-list").innerHTML = htmlItems.join("");
-    let ul = document.querySelector(".product-list")
+    let ul = document.querySelector(".product-list");
     //tag creation
-    let itemLi = document.createElement('li');
-    let itemA = document.createElement('a');
-    let itemAImg = document.createElement('img');
-    let itemA2 = document.createElement('a');
-    let itemAH2 = document.createElement('h2');
-    let itemPColor = document.createElement('p');
-    let itemPQuantity = document.createElement('p');
-    let itemPPrice = document.createElement('p');
+    let itemLi = document.createElement("li");
+    let itemA = document.createElement("a");
+    let itemAImg = document.createElement("img");
+    let itemA2 = document.createElement("a");
+    let itemAH2 = document.createElement("h2");
+    let itemPColor = document.createElement("p");
+    let itemPQuantity = document.createElement("p");
+    let itemPPrice = document.createElement("p");
     // attributes
-    itemLi.setAttribute('class', 'cart-card divider');
-    itemA.setAttribute('class', 'cart-card__image');
-    itemA.setAttribute('href', '#');
-    itemAImg.setAttribute('src', `${cartItem.Image}`);
-    itemAImg.setAttribute('alt', `${cartItem.Name}`);
-    itemA2.setAttribute('href', "#");
-    itemAH2.setAttribute('class', 'card__name');
-    itemPColor.setAttribute('class', 'cart-card__color');
-    itemPQuantity.setAttribute('class', 'cart-card__quantity');
-    itemPPrice.setAttribute('class', 'cart-card__price');
+    itemLi.setAttribute("class", "cart-card divider");
+    itemA.setAttribute("class", "cart-card__image");
+    itemA.setAttribute("href", "#");
+    itemAImg.setAttribute("src", `${cartItem.Image}`);
+    itemAImg.setAttribute("alt", `${cartItem.Name}`);
+    itemA2.setAttribute("href", "#");
+    itemAH2.setAttribute("class", "card__name");
+    itemPColor.setAttribute("class", "cart-card__color");
+    itemPQuantity.setAttribute("class", "cart-card__quantity");
+    itemPPrice.setAttribute("class", "cart-card__price");
     //textContent
     itemAH2.textContent = `${cartItem.Name}`;
-    itemPColor.textContent = `${cartItem.Colors[0].ColorName}`
-    itemPQuantity.textContent = `qty: 1`
-    itemPPrice.textContent = `${cartItem.FinalPrice}`
+    itemPColor.textContent = `${cartItem.Colors[0].ColorName}`;
+    itemPQuantity.textContent = `qty: 1`;
+    itemPPrice.textContent = `${cartItem.FinalPrice}`;
 
     //appending
-    
+
     itemLi.appendChild(itemA);
     itemA.appendChild(itemAImg);
     itemLi.appendChild(itemA2);
@@ -46,30 +45,27 @@ function getCartContents() {
     itemLi.appendChild(itemPQuantity);
     itemLi.appendChild(itemPPrice);
 
-  
     ul.appendChild(itemLi);
-
-    
   }
 }
 
 //function renderCartItem(item) {
-  //let newItem = `<li class="cart-card divider">
-  //<a href="#" class="cart-card__image">
-   // <img
-   //   src="${item.Image}"
-   //   alt="${item.Name}"
-   // />
- // </a>
-  //<a href="#">
-  //  <h2 class="card__name">${item.Name}</h2>
-  //</a>
-  //<p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  //<p class="cart-card__quantity">qty: 1</p>
- // <p class="cart-card__price">$${item.FinalPrice}</p>
+//let newItem = `<li class="cart-card divider">
+//<a href="#" class="cart-card__image">
+// <img
+//   src="${item.Image}"
+//   alt="${item.Name}"
+// />
+// </a>
+//<a href="#">
+//  <h2 class="card__name">${item.Name}</h2>
+//</a>
+//<p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//<p class="cart-card__quantity">qty: 1</p>
+// <p class="cart-card__price">$${item.FinalPrice}</p>
 //</li>`;
-  //console.log(newItem);
- // return newItem;
+//console.log(newItem);
+// return newItem;
 //}
 
 getCartContents();
