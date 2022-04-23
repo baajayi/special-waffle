45b34b71610a339943ad8b144c3c57b3ec44d49b
KendraAnderson <kor04003@byui.edu>
2022-04-22 18:22:08 -0700

    partial fix of add cart errors

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,11 +3,11 @@ function getLocalStorage(key) {
 }
 
 function getCartContents() {
-  let markup = "";
+  //let markup = "";
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+  //const htmlItems = Object.keys(cartItems).map((item) => renderCartItem(item));
+  //document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
@@ -25,7 +25,7 @@ function renderCartItem(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-  console.log(newItem);
+  //console.log(newItem);
   return newItem;
 }
 
