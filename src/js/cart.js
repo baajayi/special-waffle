fd683158d7487e9f2d0be1de0751cacc2f3bc82b
baajayi <delly31@hotmail.com>
2022-04-23 23:03:36 +0100

    initial commit

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,32 +1,75 @@
+let i = 0
 function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
 function getCartContents() {
-  let markup = "";
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+  //let markup = "";
+  for (let i = 0; i < localStorage.length; i++) { 
+    let cartItem = getLocalStorage(i+1);
+    //const htmlItems = cartItems.map((item) => renderCartItem(item));
+    //document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    let ul = document.querySelector(".product-list")
+    //tag creation
+    let listItem = document.createElement('li');
+    let aItem = document.createElement('a');
+    let itemAImg = document.createElement('img');
+    let itemA2 = document.createElement('a');
+    let itemAH2 = document.createElement('h2');
+    let itemPColor = document.createElement('p');
+    let itemPQuantity = document.createElement('p');
+    let itemPPrice = document.createElement('p');
+    // attributes
+    listItem.setAttribute('class', 'cart-card divider');
+    aItem.setAttribute('class', 'cart-card__image');
+    aItem.setAttribute('href', '#');
+    itemAImg.setAttribute('src', `${cartItem.Image}`);
+    itemAImg.setAttribute('alt', `${cartItem.Name}`);
+    itemA2.setAttribute('href', "#");
+    itemAH2.setAttribute('class', 'card__name');
+    itemPColor.setAttribute('class', 'cart-card__color');
+    itemPQuantity.setAttribute('class', 'cart-card__quantity');
+    itemPPrice.setAttribute('class', 'cart-card__price');
+    //textContent
+    itemAH2.textContent = `${cartItem.Name}`;
+    itemPColor.textContent = `${cartItem.Colors[0].ColorName}`
+    itemPQuantity.textContent = `qty: 1`
+    itemPPrice.textContent = `${cartItem.FinalPrice}`
+
+    //appending
+    
+    listItem.appendChild(aItem);
+    aItem.appendChild(itemAImg);
+    listItem.appendChild(itemA2);
+    listItem.appendChild(itemAH2);
+    listItem.appendChild(itemPColor);
+    listItem.appendChild(itemPQuantity);
+    listItem.appendChild(itemPPrice);
+
+  
+    ul.appendChild(listItem);
+
+    
+  }
 }
 
-function renderCartItem(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-  console.log(newItem);
-  return newItem;
-}
+//function renderCartItem(item) {
+  //let newItem = `<li class="cart-card divider">
+  //<a href="#" class="cart-card__image">
+   // <img
+   //   src="${item.Image}"
+   //   alt="${item.Name}"
+   // />
+ // </a>
+  //<a href="#">
+  //  <h2 class="card__name">${item.Name}</h2>
+  //</a>
+  //<p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  //<p class="cart-card__quantity">qty: 1</p>
+ // <p class="cart-card__price">$${item.FinalPrice}</p>
+//</li>`;
+  //console.log(newItem);
+ // return newItem;
+//}
 
 getCartContents();
